90929128f7b66ed99eabbdb597ceb038d6298025
axaysushir <akshay.sushir99@gmail.com>
2021-05-10 13:22:19 +0530

    Update interface

--- index.ts
+++ index.ts
@@ -18,3 +18,21 @@ class UserAccount {
 }
 const user: User = new UserAccount('jhon', 2, 'user@email.com')
 // console.log(user);
+
+
+// Unions
+type Mybool = true | false
+
+type windowState = 'open' | 'close' | 'minimized'
+type lockState = 'locked' | 'unlocked'
+
+// function for union type
+function getLength(obj: string | string[]) {
+    return obj.length
+}
+
+function wrapInArray(obj: string | string[]) {
+    if (typeof obj === 'string') {
+        return [obj]
+    } else return obj
+}
