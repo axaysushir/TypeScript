4e373411b836fa7623eb8fb6fa137b8c62e43fa2
axaysushir <akshay.sushir99@gmail.com>
2021-05-12 11:26:10 +0530

    generics in TS

--- index.ts
+++ index.ts
@@ -36,3 +36,22 @@ function wrapInArray(obj: string | string[]) {
         return [obj]
     } else return obj
 }
+
+// Generics
+
+// Generics provide variables to types. A common example is an array. An array without generics could contain anything. 
+// An array with generics can describe the values that the array contains.
+
+type StringArray = Array<string>
+type NumArray = Array<number>
+type ObjectWithNameArray = Array<{name: string}>
+
+interface Backpack<Type> {
+    add: (obj: Type) => void
+    get: () => Type
+}
+
+declare const backpack: Backpack<string>
+
+const object = backpack.get()
+backpack.add(23)
