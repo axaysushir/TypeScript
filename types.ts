cd68182b6156203e7f3986a0a73ff964295f8a65
axaysushir <akshay.sushir99@gmail.com>
2021-05-19 19:07:43 +0530

    Conditional types

--- types.ts
+++ types.ts
@@ -13,3 +13,17 @@ type ex1 = Dog extends Animal ? number: string; // number
 type ex2 = RegExp extends Animal ? number : string; // string
 
 // Conditional types take a form that looks a little like conditional expressions (condition ? trueExpression : falseExpression) in JavaScript:
+
+// with generics
+interface idlabel {
+    id: number;
+}
+interface namelabel {
+    name: string;
+}
+
+function createLabel(id: number): idlabel;
+function createLabel(name: string): namelabel;
+function createLabel(nameOrid: number | string): idlabel | namelabel {
+    throw "unimplemented"
+}
