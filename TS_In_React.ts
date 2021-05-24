08d920b9ecdce563855af8f1db618873c8bfb9ea
axaysushir <akshay.sushir99@gmail.com>
2021-05-24 19:08:11 +0530

    Aarray des

--- TS_In_React.ts
+++ TS_In_React.ts
@@ -5,8 +5,25 @@ interface UserThumbnailProps {
     url: string
 }
 
-export const UserThumbnail = (props = userThumbnailProps) => {
-    <a href={props.url}>
-        <img src={props.img} alt={props.alt} />
-    </a>
-}
+// export const UserThumbnail = (props = userThumbnailProps) => {
+//     <a href={props.url}>
+//         <img src={props.img} alt={props.alt} />
+//     </a>
+// }
+
+// Array destructuring
+
+let input = [1,2,3]
+// let [first, sec] = input
+// console.log(first);
+// console.log(sec);
+
+let [first, ...rest] = [1,2,3,4,5]
+console.log(first);
+console.log(rest);
+
+
+// Tupel destructuring
+let tuple: [number, string, boolean] = [7, "hello", true];
+
+let [a, b, c] = tuple; // a: number, b: string, c: boolean
