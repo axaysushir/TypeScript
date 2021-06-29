848c7fe9990a1c9df59c630a1b257baaadc55950
axaysushir <akshay.sushir99@gmail.com>
2021-06-29 11:23:31 +0530

    TS in react

--- TS_In_React.ts
+++ TS_In_React.ts
@@ -27,3 +27,44 @@ console.log(rest);
 let tuple: [number, string, boolean] = [7, "hello", true];
 
 let [a, b, c] = tuple; // a: number, b: string, c: boolean
+
+// functional components
+type AppProps = {
+    message: string;
+}
+
+const App = ({message}: AppProps) => <div>{message}</div>;
+
+const App2 = ({message}: AppProps): JSX.Element => <div>{message}</div>;
+// inline
+const app = ({message}: {message: string}) => <div>{message}</div>;
+
+// use reducer
+
+const initialState = {count: 0}
+
+type ACTIONTYPE = 
+    | {type: "increment"; payload: number}
+    | {type: "decrement"; payload: string};
+
+function reducer(state: typeof initialState, action: ACTIONTYPE) {
+    switch (action.type) {
+        case "increment":
+            return {count: state.count + action.payload}
+        case "decrement":
+            return {count: state.count - Number(action.payload)}
+        default:
+            throw new Error();
+    }
+}
+
+function Counter() {
+    const [state, dispatch] = React.useReducer(reducer, initialState)
+    return (
+        <>
+            Count: {state.count}
+            <button onClick={() => dispatch({type: "decrement", payload: "5"})}>-</button>
+            <button onClick={() => dispatch({type: "increment", payload: 5})}>+</button>
+        </>
+    )
+}
